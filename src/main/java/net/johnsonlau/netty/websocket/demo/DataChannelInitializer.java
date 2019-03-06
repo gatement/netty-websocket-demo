612a3c5b99b7b3c3ba194c7bb3324e87f8b74f7e
liugh22 <guanhua.liu@midea.com>
2019-03-06 15:20:34 +0800

    go

--- src/main/java/net/johnsonlau/netty/websocket/demo/DataChannelInitializer.java
+++ src/main/java/net/johnsonlau/netty/websocket/demo/DataChannelInitializer.java
@@ -7,10 +7,13 @@ import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class DataChannelInitializer extends ChannelInitializer<Channel> {
     @Override
     protected void initChannel(Channel channel) throws Exception {
+    	log.info("DataChannelInitializer.initChannel: {}", this.hashCode());
         ChannelPipeline pipeline = channel.pipeline();
         pipeline.addLast("http-codec", new HttpServerCodec());
         pipeline.addLast("aggregator", new HttpObjectAggregator(65536));
--- src/main/java/net/johnsonlau/netty/websocket/demo/MyChannelInboundHandler.java
+++ src/main/java/net/johnsonlau/netty/websocket/demo/MyChannelInboundHandler.java
@@ -8,9 +8,15 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MyChannelInboundHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+    	log.info("MyChannelInboundHandler.channelActive: {}", this.hashCode());
+        ctx.fireChannelActive();
+    }
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame msg) throws Exception {
+    	log.info("MyChannelInboundHandler.channelRead0: {}", this.hashCode());
 		if (msg instanceof TextWebSocketFrame) {
 			TextWebSocketFrame textMsg = (TextWebSocketFrame) msg;
 			log.info(textMsg.text());
